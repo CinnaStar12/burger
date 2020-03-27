4552ea2152109fc60e81631d3e9611d10d0519ac
Allan Binayao <allan.binayao@gmail.com>
2020-03-27 15:48:33 -0500

    model and controller finished up for now, views started

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,32 @@
+var express = require("express")
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        var hbsObj = {
+            burgers: data
+        };
+        console.log(hbsObj);
+        res.render("index", hbsObj);
+    })
+})
+
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne("burger_name", req.body.burger_name, function(result) {
+        res.json({ id: result.insertId });
+    })
+})
+
+router.delete("/api/burgers/:id", function(req, res){
+    burger.updateOne("devoured", false, "id", req.params.id, function(result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        }
+        res.status(200).end();
+    } )
+})
+
+module.exports = router;
